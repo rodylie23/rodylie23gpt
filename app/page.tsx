04634028415d9a4caaa1e98a5c757a2601a461e2
giftedtech <admin@giftedtechnexus.co.ke>
2024-03-24 00:42:47 +0300

    𝗚𝗜𝗙𝗧𝗘𝗗 𝗖𝗛𝗔𝗧𝗚𝗣𝗧

--- app/page.tsx
+++ app/page.tsx
@@ -100,7 +100,7 @@ export default function Chat() {
         <div className="border-gray-200sm:mx-0 mx-5 mt-20 max-w-screen-md rounded-md border sm:w-full">
           <div className="flex flex-col space-y-4 p-7 sm:p-10">
             <h1 className="text-lg font-semibold text-black">
-              Hi👋 I Am Gifted ChatGPT!
+              Hi👋 I Am Gifted ChatGPT-4!
             </h1>
             <p className="text-gray-500">
               I Am Programmed by{" "}
@@ -119,7 +119,7 @@ export default function Chat() {
                 rel="noopener noreferrer"
                 className="font-medium underline underline-offset-4 transition-colors hover:text-black"
               >
-                OpenAI APi Functions
+                OpenAI GPT-4 APi Functions
               </a>{" "}
               and{" "}
               <a
